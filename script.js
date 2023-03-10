af1d2b94e401c9721654e667577e1d09111e4c06
Jorge Lemes <you@example.com>
2023-03-10 09:14:23 -0300

    from contato

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+/* function useEffect(name) {
+    console.log('antes de axecutar a função callback')
+    
+    name()
+    
+    console.log('depois de executar a callback')
+}
+
+useEffect(
+    () => {
+        console.log('estou em um callback')
+    }
+) */
+
+
+
+
+
+/* function Person(name){
+    this.name = name;
+    this.walk = function(){
+
+        return 'andando'
+
+    }
+
+}
+
+const mayk = new Person('mayk');
+const jor = new Person('jor');
+console.log(mayk.walk())
+console.log(jor.walk()) */
+
+
+let date = new Date("09-03-2023")
+
+console.log(date)
